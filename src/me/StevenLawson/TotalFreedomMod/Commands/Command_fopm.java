c6a392d0e22b8f080ed63937394c9df599a07087
Cameron Redmore <admin@cam-tech.net>
2014-08-05 20:13:37 +0100

    Added /fopm

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_fopm.java
@@ -0,0 +1,72 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Commands.AdminLevel;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandParameters;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions;
+import me.StevenLawson.TotalFreedomMod.Commands.SourceType;
+import me.StevenLawson.TotalFreedomMod.Commands.TFM_Command;
+import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_BanManager;
+import me.StevenLawson.TotalFreedomMod.TFM_CommandBlocker;
+import me.StevenLawson.TotalFreedomMod.TFM_Log;
+import me.StevenLawson.TotalFreedomMod.TFM_PermbanList;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerList;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(description = "Shows information about TotalFreedomMod or reloads it", usage = "/<command> [reload]")
+public class Command_fopm extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 1)
+        {
+            if (!args[0].equals("reload"))
+            {
+                return false;
+            }
+
+            if (!TFM_AdminList.isSuperAdmin(sender))
+            {
+                playerMsg(TotalFreedomMod.MSG_NO_PERMS);
+                return true;
+            }
+
+            TFM_AdminList.load();
+            TFM_PermbanList.load();
+            TFM_PlayerList.load();
+            TFM_BanManager.load();
+            TFM_CommandBlocker.load();
+
+
+            final String message = String.format("%s v%s.%s reloaded.",
+                    TotalFreedomMod.pluginName,
+                    TotalFreedomMod.pluginVersion,
+                    TotalFreedomMod.buildNumber);
+
+            playerMsg(message);
+            TFM_Log.info(message);
+            return true;
+        }
+
+        playerMsg("Welcome to the FreedomOpMod; our server's main plugin!", ChatColor.GOLD);
+        playerMsg("Created by Camzie99", ChatColor.GOLD);
+        StringBuilder developers = new StringBuilder();
+        developers.append("Later worked on by: CrafterSmith12");
+        for (String dev : TFM_Util.FOP_DEVELOPERS)
+        {
+            developers.append(", " + dev);
+        }
+        playerMsg(developers.toString(), ChatColor.AQUA);
+        playerMsg("Visit " + ChatColor.AQUA + "http://freedomop.boards.net/" + ChatColor.GREEN + " to visit our forums and get support!", ChatColor.GREEN);
+
+        return true;
+    }
+}
