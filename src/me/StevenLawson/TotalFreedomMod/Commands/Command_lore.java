57a0ddea42cf5155d300828dc7ba029fd7644380
Cameron Redmore <admin@cam-tech.net>
2014-08-05 21:40:02 +0100

    Added myself as buildcreator, Added a lore switching command Added a /sys

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_lore.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_lore.java
@@ -1,5 +1,6 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
+import java.util.Arrays;
 import java.util.List;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
@@ -13,25 +14,39 @@ import org.bukkit.inventory.meta.ItemMeta;
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(
         description = "Add Lore to the current item in your hand.",
-        usage = "/<command> <line> <lore>",
+        usage = "/<command> <lore1> <lore2> <lore3> <lore4>",
         aliases = "lr")
 public class Command_lore extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if (args.length < 2)
+        if (args.length < 1)
         {
             return false;
         }
-        String loreRaw = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
-        String itemLore = TFM_Util.colorize(loreRaw.trim());
         ItemStack i = sender_p.getItemInHand();
         if (i != null)
         {
             ItemMeta im = i.getItemMeta();
-            List<String> lore = im.getLore();
-            lore.set(Integer.parseInt(args[0]), itemLore);
+            List<String> lore;
+            if (args.length == 1)
+            {
+                lore = Arrays.asList(TFM_Util.colorize(args[0]));
+            }
+            else if (args.length == 2)
+            {
+                lore = Arrays.asList(TFM_Util.colorize(args[0]), TFM_Util.colorize(args[1]));
+            }
+            else if (args.length == 3)
+            {
+                lore = Arrays.asList(TFM_Util.colorize(args[0]), TFM_Util.colorize(args[1]), TFM_Util.colorize(args[2]));
+            }
+            else
+            {
+                lore = Arrays.asList(TFM_Util.colorize(args[0]), TFM_Util.colorize(args[1]), TFM_Util.colorize(args[2]), TFM_Util.colorize(args[3]));
+            }
+            im.setLore(lore);
             i.setItemMeta(im);
         }
         return true;
