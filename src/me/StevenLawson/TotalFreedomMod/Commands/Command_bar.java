926b7c6c4f0594c7434c285d9aad2e9359019dfa
Dillon k <supitsdillon@net.dut.edu.vn>
2014-08-07 11:00:55 +0000

    Command_bar.java created online with Bitbucket

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_bar.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_bar.java
@@ -1,41 +1,66 @@
+//Taken  from the old FOPMod source
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
 import me.confuser.barapi.BarAPI;
 import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+/*
+  When compilng please use http://dev.bukkit.org/bukkit-plugins/bar-api/files/5-bar-api-v3-1/
+ else it won't work c:
+*/
 
-@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
-@CommandParameters(
-        description = "Alter what is currently on the boss bar.",
-        usage = "/<command> [clear | message]")
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandParameters(description = "Change the bar message or clear it", usage = "/bar [clear | message]")
 public class Command_bar extends TFM_Command
 {
+  /*
+  When compilng please use http://dev.bukkit.org/bukkit-plugins/bar-api/files/5-bar-api-v3-1/
+  Credit to PieGuy
+ */
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if (args.length < 1)
+        if (!server.getPluginManager().isPluginEnabled("BarAPI"))
         {
-            TFM_Util.playerMsg(sender, "Invalid number of arguments.", ChatColor.RED);
+            playerMsg("BarAPI is not enabled on this server");
+            return true;
+        }
+
+        if (args.length == 0)
+        {
             return false;
         }
+        
         if (args[0].equalsIgnoreCase("clear"))
         {
             for (Player player : server.getOnlinePlayers())
             {
                 BarAPI.removeBar(player);
             }
-            TFM_Util.adminChatMessage(sender, "[BAR-API] Bar Cleared.", false);
         }
         else
-        {
+        {        
             String message = StringUtils.join(ArrayUtils.subarray(args, 0, args.length), " ");
-            BarAPI.setMessage(TFM_Util.colorize(message), 60);
-            TFM_Util.adminChatMessage(sender, "[BAR-API] Bar message changed.", false);
+            BarAPI.setMessage(message.replaceAll("&", "§"), 60);
+            if (args.length == 1)
+                for (int i = 1; i < args.length; i++)
+                {
+                    if (i > 1)
+                    {
+                        message += " ";
+                    }
+                    message += args[i];
+                }
+                BarAPI.setMessage(message.replaceAll("&", "§"), 60);
+                TFM_Util.bcastMsg(TotalFreedomMod.FREEDOMOP_MODBROADCAST + ChatColor.GREEN + message);
+
         }
         return true;
     }
