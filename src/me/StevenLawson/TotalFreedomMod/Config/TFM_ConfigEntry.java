35d3ec2e145001d976b278ef18fbe0493c4500e1
Pie Guy <pieguy7896>
2014-08-11 11:18:14 +0000

    TFM_ConfigEntry.java edited online with Bitbucket

--- src/me/StevenLawson/TotalFreedomMod/Config/TFM_ConfigEntry.java
+++ src/me/StevenLawson/TotalFreedomMod/Config/TFM_ConfigEntry.java
@@ -29,6 +29,7 @@ public enum TFM_ConfigEntry
     SERVER_ADDRESS(String.class, "server.address"),
     SERVER_MOTD(String.class, "server.motd"),
     SERVER_OWNERS(List.class, "server.owners"),
+    DEVELOPERS(List.class, "server.developers"),
     SERVER_BAN_URL(String.class, "server.ban_url"),
     SERVER_PERMBAN_URL(String.class, "server.permban_url"),
     //
