e02544a95513f734db8f4fb4d9e08eee7aa46f51
John Miller <b069b636@opayq.com>
2014-08-22 02:29:54 +0000

    Disabled WorldEdit rollbacks and TFM_RollbackManager due to CoreProtect replaced it.   I've also changed it so CoreProtect will not spam the administrators anymore.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
@@ -42,9 +42,15 @@ public class Command_gtfo extends TFM_Command
         }
 
         TFM_Util.bcastMsg(player.getName() + " has been a VERY naughty, naughty boy.", ChatColor.RED);
-        server.dispatchCommand(sender, "co rb u:" + player.getName() + " t:24h r:global");
+        
+        // Silently rollback the user with CoreProtect
+        server.dispatchCommand(sender, "co rb u:" + player.getName() + " t:24h r:global #silent");
+        
+        
+        // Disabled TFM RollbackManager and WorldEdit undones due to Coreprotect is handle it now.
         // Undo WorldEdits:
-        try
+       
+ /*       try
         {
             TFM_WorldEditBridge.undo(player, 15);
         }
@@ -54,7 +60,7 @@ public class Command_gtfo extends TFM_Command
 
         // rollback
         TFM_RollbackManager.rollback(player.getName());
-
+*/
         // deop
         player.setOp(false);
 
