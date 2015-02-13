bb74b105c8194d844edf19f5a2e8e4a87347d848
theLAZYway <theLAZYway>
2015-02-13 02:19:32 +0000

    Command_gtfo.java Edits: Changed Bcast Msg, Now displays the command sender name

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
@@ -79,7 +79,7 @@ public class Command_gtfo extends TFM_Command
 
         // ban IP address:
         String ip = TFM_Util.getFuzzyIp(player.getAddress().getAddress().getHostAddress());
-        TFM_Util.bcastMsg(String.format("Banning: %s, IP: %s ", player.getName(), ip) + (reason != null ? ("- Reason: " + ChatColor.YELLOW + reason) : ""), ChatColor.RED);
+        TFM_Util.bcastMsg(String.format(sender.getname() " - Banning: %s, IP: %s ", player.getName(), ip) + (reason != null ? ("- Reason: " + ChatColor.YELLOW + reason) : ""), ChatColor.RED);
 
         TFM_BanManager.addIpBan(new TFM_Ban(ip, player.getName(), sender.getName(), null, reason));
 
