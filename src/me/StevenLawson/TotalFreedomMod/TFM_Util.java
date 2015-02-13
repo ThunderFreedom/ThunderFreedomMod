71ce3581a9574492b7c1ca0a4431ac5431e8f522
theLAZYway <theLAZYway>
2015-02-13 17:06:59 +0000

    TFM_Util.java edited online with Bitbucket
    Fixed PhoenixBlader's Edit:
    Removed theLAZYway01, I Name Changed to DaPancake
    Added group Developers and renamed original group to TFM_Developers
    Added DaPancake & ThePheonixBlader

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -69,8 +69,9 @@ public class TFM_Util
 {
     private static final Map<String, Integer> ejectTracker = new HashMap<String, Integer>();
     public static final Map<String, EntityType> mobtypes = new HashMap<String, EntityType>();
-    public static final List<String> DEVELOPERS = Arrays.asList("theLAZYway01", "Madgeek1450", "DarthSalamon", "AcidicCyanide", "wild1145", "WickedGamingUK");
-    public static final List<String> FOP_DEVELOPERS = Arrays.asList("theLAZYway01", "Paldiu", "RobinGall2910", "Freelix2000", "PieGuy7896");
+    public static final List<String> TFM_DEVELOPERS = Arrays.asList("Madgeek1450", "DarthSalamon", "AcidicCyanide", "wild1145", "WickedGamingUK");
+    public static final List<String> FOP_DEVELOPERS = Arrays.asList("Paldiu", "RobinGall2910", "Freelix2000", "PieGuy7896");
+    public static final List<String> DEVELOPERS = Arrays.asList("ThePheonixBlader", "DaPancake");
     public static final List<String> WEB_DEVELOPERS = Arrays.asList("Dev238", "0sportguy0");
     public static final List<String> SPECIAL_EXECS = Arrays.asList("aggelosQQ", "UltimaTheHawke", "_GeneralOfGames_", "zthehorsekid");
     public static final List<String> SYS_ADMINS = Arrays.asList("reuben4545", "Elmon11", "jspence23", "decyj145", "dbooj", "falceso", "ThePheonixBlader", "lynxlps", "cowgomooo12", "Camzie99", "CrafterSmith12", "DarkLynx108");
